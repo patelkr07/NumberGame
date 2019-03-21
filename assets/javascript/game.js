baa53dd932d461f2ab6303f5ff062121f633dfa1
Kunal <patelkr07@gmail.com>
2019-03-20 20:35:13 -0500

    Click event placeholders

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+
+let ewokRandom = [];
+
+let leiaRandom = [];
+
+let speederRandom = [];
+
+let vaderRandom = [];
+
+let npcRandom = [];
+
+let userScore = [];
+
+let wins = [];
+
+let losses = [];
+
+$(document).ready(function() {
+
+    $(".ewok").on("click", function() { 
+        alert("Ewok been clicked");
+    });
+
+    $(".leia").on("click", function() { 
+        alert("Leia been clicked");
+    });
+
+    $(".speeder").on("click", function() { 
+        alert("Speeder been clicked");
+    });
+
+    $(".vader").on("click", function() { 
+        alert("Vader been clicked");
+    });
+});
+
+
+
+
